081b0972cca9d899a326e385efde095bbe2556cf
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-10 03:57:59 +0900

    add ArticleAddActivity.java ソース追加

--- ItArticleCollection40024/app/src/main/java/local/hal/st32/android/itarticlecollection40024/ArticleAddActivity.java
+++ ItArticleCollection40024/app/src/main/java/local/hal/st32/android/itarticlecollection40024/ArticleAddActivity.java
@@ -1,8 +1,43 @@
-package local.hal.st32.android.itarticlecollection40024;
-
 /**
  * Created by Tester on 2017/01/10.
+ * content: 新規登録ボタンを押された際に表示するアクティビティ
  */
+package local.hal.st32.android.itarticlecollection40024;
 
-public class ArticleAddActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class ArticleAddActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_add);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.article_add_meun, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId=item.getItemId();
+        switch (itemId){
+            case R.id.menuInsert:
+
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
