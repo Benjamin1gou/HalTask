f41924f4e5504579056dafd2f087a9818da0d9d1
ueshima <re-to-mikan@hotmail.co.jp>
2017-01-10 03:34:56 +0900

    add ArticleDetailActivity.java	Activityクラス作成

---
+++ ItArticleCollection40024/app/src/main/java/local/hal/st32/android/itarticlecollection40024/ArticleDetailActivity.java
@@ -0,0 +1,19 @@
+package local.hal.st32.android.itarticlecollection40024;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tester on 2017/01/10.
+ * content: リストアイテムが押された際に表示する詳細情報のActivity
+ */
+
+public class ArticleDetailActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_detail);
+    }
+
+}
